fe827fe017389bfb4a09ea2058a4cb4a6b73e03f
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 13:43:23 -0700

    asdf

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -1,28 +1,36 @@
 var menu = document.getElementById("rclickmenu");
 document.addEventListener("contextmenu",async function(e){
     e.preventDefault();
-    if(e.button === 2){
+    if (e.button === 2) {
         menu.style.display = "block";
         menu.style.transition = "0.5s";
-        menu.style.backgroundColor = "rgba(90,90,90,0)";
-        menu.style.backdropFilter= "blur(0px)";
-        await POSH.pause(510);
-        menu.style.transition = "0s";
-        menu.style.display = "none";
-        menu.style.left = e.pageX+"px";
-        menu.style.top = e.pageY+"px";
-        await POSH.pause(1);
-        menu.style.display = "block";
-        menu.style.transition = "0.5s";
-        menu.style.backdropFilter= "blur(6px)";
-        await POSH.pause(500);
-        menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
-        await POSH.pause(500);
+        menu.style.backgroundColor = "rgba(90, 90, 90, 0)";
+        menu.style.backdropFilter = "blur(0px)";
+        menu.style.left = e.pageX + "px";
+        menu.style.top = e.pageY + "px";
+    
+        setTimeout(async () => {
+            menu.style.transition = "0s";
+            menu.style.display = "none";
+    
+            await POSH.pause(1);
+    
+            menu.style.display = "block";
+            menu.style.transition = "0.5s";
+            menu.style.backdropFilter = "blur(6px)";
+            menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)";
+    
+            await POSH.pause(500);
+        }, 510);
     }
-    if(e.button === 0){
+    
+    if (e.button === 0) {
         console.log("click");
-        menu.style.backgroundColor = "rgba(90,90,90,0)";
-        await POSH.pause(500);
-        menu.style.display = "none";
+        menu.style.backgroundColor = "rgba(90, 90, 90, 0)";
+    
+        setTimeout(() => {
+            menu.style.display = "none";
+        }, 500);
     }
+    
 });
