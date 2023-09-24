810e15938399008beba5fe1bb31987d3fe062f7a
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 17:31:52 +0000

    styles and styles

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -2,7 +2,6 @@ var menu = document.getElementById("rclickmenu");
 document.addEventListener("contextmenu",async function(e){
     if(e.button===2){
         e.preventDefault();
-        console.log("rclick");
         menu.style.display = "none";
         menu.style.transition = "0s";
         menu.style.left = e.pageX+"px";
@@ -11,7 +10,8 @@ document.addEventListener("contextmenu",async function(e){
         menu.style.display = "block";
         menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
     }else if(e.button === 0){
-        menu.style.display = "none";
         menu.style.backgroundColor = "rgba(90,90,90,0)";
+        await POSH.pause(500);
+        menu.style.display = "none";
     }
 });
