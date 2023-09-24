a839295bfc9bfac2e72511df5369f927c6f6d62d
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 17:44:38 +0000

    put pause after backgroundColor

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -5,13 +5,16 @@ document.addEventListener("contextmenu",async function(e){
         menu.style.display = "block";
         menu.style.transition = "0.5s";
         menu.style.backgroundColor = "rgba(90,90,90,0)";
+        await POSH.pause(500);
         menu.style.transition = "0s";
+
         menu.style.left = e.pageX+"px";
         menu.style.top = e.pageY+"px";
         await POSH.pause(0);
         menu.style.display = "block";
         menu.style.transition = "0.5s";
         menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
+        await POSH.pause(500);
         menu.style.transition = "0s";
     }else if(e.button===0){
         menu.style.backgroundColor = "rgba(90,90,90,0)";
