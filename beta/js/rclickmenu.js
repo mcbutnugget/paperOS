f58e4a977576623314c21ab105d7dcb8a943cf56
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 14:10:26 -0700

    went backwards, I think it's been fixed

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -1,49 +1,29 @@
 var menu = document.getElementById("rclickmenu");
-var menuVisible = false;
-
-document.addEventListener("contextmenu", function (e) {
-    e.preventDefault(); // Prevent the default context menu
-    var mouseX = e.pageX;
-    var mouseY = e.pageY;
-
-    if (menuVisible) {
-        // Hide the menu with a fade-out effect
+document.addEventListener("contextmenu",async function(e){
+    e.preventDefault();
+    if(e.button === 2){
+        menu.style.display = "block";
         menu.style.transition = "0.5s";
-        menu.style.opacity = "0";
-
-        setTimeout(function () {
-            // After the fade-out animation, hide the menu
-            menu.style.transition = "0s"; // Disable transition for instant teleportation
-            menu.style.display = "none";
-        }, 500); // Adjust the timeout duration to match the fade-out duration
-
-        menuVisible = false;
-    } else {
-        // Teleport the menu to the mouse position and make it visible
-        menu.style.transition = "0s"; // Disable transition for instant teleportation
-        menu.style.left = mouseX + "px";
-        menu.style.top = mouseY + "px";
-        menu.style.backgroundColor = "rgba(90, 90, 90, 0.7)"; // Set background to semi-transparent
-        menu.style.opacity = "1"; // Make it fully visible
-        menu.style.display = "block"; // Ensure it's displayed
-
-        menuVisible = true;
+        menu.style.backgroundColor = "rgba(90,90,90,0)";
+        menu.style.backdropFilter= "blur(0px)";
+        menu.style.display = "none"
+        await POSH.pause(510);
+        menu.style.transition = "0s";
+        menu.style.display = "none";
+        menu.style.left = e.pageX+"px";
+        menu.style.top = e.pageY+"px";
+        await POSH.pause(1);
+        menu.style.display = "block";
+        menu.style.transition = "0.5s";
+        menu.style.backdropFilter= "blur(6px)";
+        await POSH.pause(500);
+        menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
     }
 });
 
-// Handle left-click to hide the menu
-document.addEventListener("click", function (e) {
-    if (e.button === 0 && menuVisible) {
-        // Hide the menu with a fade-out effect
-        menu.style.transition = "0.5s";
-        menu.style.opacity = "0";
-
-        setTimeout(function () {
-            // After the fade-out animation, hide the menu
-            menu.style.transition = "0s"; // Disable transition for instant teleportation
-            menu.style.display = "none";
-        }, 500); // Adjust the timeout duration to match the fade-out duration
-
-        menuVisible = false;
-    }
-});
+document.addEventListener("click", async function(e){
+        console.log("click");
+        menu.style.backgroundColor = "rgba(90,90,90,0)";
+        await POSH.pause(500);
+        menu.style.display = "none";
+})
