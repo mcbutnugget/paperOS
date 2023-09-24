22f77a750ca442f9c32029dd34fc944cc5e17152
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 13:34:08 -0700

    changed 500 to 510

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -6,9 +6,8 @@ document.addEventListener("contextmenu",async function(e){
         menu.style.transition = "0.5s";
         menu.style.backgroundColor = "rgba(90,90,90,0)";
         menu.style.backdropFilter= "blur(0px)";
-        await POSH.pause(500);
+        await POSH.pause(510);
         menu.style.transition = "0s";
-        await POSH.pause(1);
         menu.style.display = "none";
         menu.style.left = e.pageX+"px";
         menu.style.top = e.pageY+"px";
@@ -19,7 +18,8 @@ document.addEventListener("contextmenu",async function(e){
         await POSH.pause(500);
         menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
         await POSH.pause(500);
-    }else if(e.button===0){
+    }else if(e.button === 0){
+        console.log("click");
         menu.style.backgroundColor = "rgba(90,90,90,0)";
         await POSH.pause(500);
         menu.style.display = "none";
