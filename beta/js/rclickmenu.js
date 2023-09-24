d443829cf8346af8fdf6ed1e00db8107ea3c450d
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 06:41:55 +0000

    changed color from 20 to 90 and made it more opaqe,

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -1,21 +1,20 @@
-
-document.addEventListener("click",function(e){
-    var menu = document.getElementById("rclickmenu");
+var menu = document.getElementById("rclickmenu");
+document.addEventListener("click",async function(e){
     if(e.type=="2"){
         console.log("rclick");
-        menu.style.top = e.clientY;
-        menu.style.left = e.clientY;
+        menu.style.top = e.clientY+"px";
+        menu.style.left = e.clientY+"px";
         menu.style.display = "block";
-        menu.style.backgroundColor = "rgba(20, 20, 20, 0.699)"
-        POSH.pause(500);
+        menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
+        await POSH.pause(500);
         menu.style.backdropFilter= "blur(10px)"; 
-        POSH.pause(800);
+        await POSH.pause(800);
         menu.style.backdropFilter= "blur(2px)"; 
-        POSH.pause(1000);
+        await POSH.pause(1000);
         menu.style.backdropFilter= "blur(6px)"; 
         e.preventDefault();
     }else if(e.type = "0"){
         menu.style.display = "none";
-        menu.style.backgroundColor = "rgba(20,20,20,0)";
+        menu.style.backgroundColor = "rgba(90,90,90,0)";
     }
 });
