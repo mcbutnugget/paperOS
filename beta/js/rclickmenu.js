ba826570b0764ad7e3d70847f0f6f9f0f9bef86b
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-09-24 17:38:24 +0000

    asdfasfd

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -1,15 +1,16 @@
 var menu = document.getElementById("rclickmenu");
 document.addEventListener("contextmenu",async function(e){
-    if(e.button===2){
-        e.preventDefault();
+    e.preventDefault();
+    if(e.button === 2){
         menu.style.display = "none";
         menu.style.transition = "0s";
         menu.style.left = e.pageX+"px";
         menu.style.top = e.pageY+"px";
+        await POSH.pause(1);
         menu.style.transition = "0.5s";
         menu.style.display = "block";
         menu.style.backgroundColor = "rgba(90, 90, 90, 0.8)"
-    }else if(e.button === 0){
+    }else if(e.button===0){
         menu.style.backgroundColor = "rgba(90,90,90,0)";
         await POSH.pause(500);
         menu.style.display = "none";
