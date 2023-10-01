ef192ba425031dcc00b0d749cab78b4b3054b2de
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-01 00:10:59 -0700

    i mabye fixed it

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -2,9 +2,9 @@ var menu = document.getElementById("rclickmenu");
 document.addEventListener("contextmenu",async function(e){
     e.preventDefault();
     if(e.button === 2){
+        menu.style.backgroundColor = "rgba(90,90,90,0)";
         menu.style.display = "block";
         menu.style.transition = "0.5s";
-        menu.style.backgroundColor = "rgba(90,90,90,0)";
         await POSH.pause(500);
         menu.style.backdropFilter= "blur(0px)";
         await POSH.pause(500);
