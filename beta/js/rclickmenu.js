895137d27c529d1757b2a833ccaef91cebc9cb3d
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-01 00:17:01 -0700

    i mabye?

--- beta/js/rclickmenu.js
+++ beta/js/rclickmenu.js
@@ -2,9 +2,11 @@ var menu = document.getElementById("rclickmenu");
 document.addEventListener("contextmenu",async function(e){
     e.preventDefault();
     if(e.button === 2){
+        menu.style.display = "block";
         menu.style.transition = "0.5s";
         menu.style.backgroundColor = "rgba(90,90,90,0)";
-        menu.style.backdropFilter= "blur(0px)";
+        await POSH.pause(500);
+        menu.style.display = "none";
         await POSH.pause(500);
         menu.style.transition = "0s";
         menu.style.left = e.pageX+"px";
