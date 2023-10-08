ccf1d4cd3ee3e6c3d4724c93ae6906a700d33b75
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-07 22:22:41 -0700

    why did I put the semicolon in the string?

--- beta/js/apps.js
+++ beta/js/apps.js
@@ -111,11 +111,11 @@ function openSettings(){
 
 function Scroll(){
     if(scrollOpen==false){
-        document.getElementById('overlay').style.backgroundColor="rgba(32, 32, 32, 0.7);"
+        document.getElementById('overlay').style.backgroundColor="rgba(32, 32, 32, 0.7)";
         document.getElementById('overlay').style.top="0";
         scrollOpen=true;
     }else{
-        document.getElementById('overlay').style.backgroundColor="rgba(32, 32, 32, 0);"
+        document.getElementById('overlay').style.backgroundColor="rgba(32, 32, 32, 0)";
         document.getElementById('overlay').style.top="100%";
         scrollOpen=false;
     }
