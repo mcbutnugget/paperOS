a44e6a0c80353135a77bbbab7c9ac2d91bc7c0d3
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-11 10:40:31 -0700

    should work

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,7 +1,4 @@
 function search(){
     var search = document.getElementById("search");
-    var searchResults = await fetch(search.value);
-    var searchResultsHTML = await searchResults.text();
-
-    document.getElementById("body4").innerHTML = searchResultsHTML;
+    document.getElementById("body4").innerHTML = readDisc(search.value).replace("<body>","<div>").replace("<head>","");
 }
