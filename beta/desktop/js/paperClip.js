5a51d6ad663b933171770355e699ac7dba6995c9
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-10 20:43:19 -0700

    added .value thing to this

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,4 +1,4 @@
-function search(){
+async function search(){
     var search = document.getElementById("search");
-    document.getElementById("body4").innerHTML = readDisk(search);
+    document.getElementById("body4").innerHTML = readDisk(search.value);
 }
