c537d56003d5c02bc931e5f99b1f239e0dfc737b
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-10 20:53:37 -0700

    put it in an iframe

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,4 +1,4 @@
 async function search(){
     var search = document.getElementById("search");
-    document.getElementById("body4").innerHTML = readDisk(search.value);
+    document.getElementById("body4").innerHTML = "<iframe src= `data:text/html,"+readDisk(search.value)+"`/>";
 }
