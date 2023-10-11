37ee250bc2b9b1c8d631989e528cafa377140987
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-11 11:13:14 -0700

    added the / things

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,4 +1,4 @@
 function search(){
     var search = document.getElementById("search");
-    document.getElementById("body4").innerHTML = readDisk(search.value).replace("<body>","<div>").replace("<head>","");
+    document.getElementById("body4").innerHTML = readDisk(search.value).replace("<body>","<div>").replace("<head>","").replace("</body>","</div>").replace("</head>","");
 }
