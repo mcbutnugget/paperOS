19edb8fbf999617cea3c313d218c9f1a6bf212ea
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-13 08:03:06 -0700

    yay!

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,4 +1,4 @@
 function search() {
     var search = document.getElementById("search");
-    document.getElementById("body4").innerHTML=`<iframe src='${search}'/>`;
+    document.getElementById("body4").innerHTML=`<iframe style='width:100%; height:100%;' src='${search}'/>`;
 }
