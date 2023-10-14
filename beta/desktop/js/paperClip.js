fa58d26683fcb78da3ecb31b23511f2e959fe4ac
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-14 14:46:02 -0700

    set the border width in the irame to 0

--- beta/desktop/js/paperClip.js
+++ beta/desktop/js/paperClip.js
@@ -1,4 +1,4 @@
 function search() {
     var search = document.getElementById("search");
-    document.getElementById("body4").innerHTML=`<iframe style='width:100%; height:100%;' src='${search.value}'/>`;
+    document.getElementById("body4").innerHTML=`<iframe style='width:100%; height:100%; border-width:0;' src='${search.value}'/>`;
 }
