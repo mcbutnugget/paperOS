d1fcbe0b6599cdf300d8e264bcf2b2f9a66176ea
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-16 20:14:45 -0700

    asdfasdfasdf

--- beta/desktop/php/webBrowser.php
+++ beta/desktop/php/webBrowser.php
@@ -1,5 +1,7 @@
 <div>
     <?php
-        require "https://posh.glitch.me/"
+        $file = fopen("https://posh.glitch.me/", "r") or die("I DIED ECAUSE CANT OPNEN DA FILE");
+        echo fread($file, fread("https://posh.glitch.me/"));
+        fclose($file);
     ?>
 </div>
