15cb0d1d92634dee700862559a36fca8a0aaf273
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-19 07:57:38 -0700

    this should work now, what it does is instead of setting the SRC to that, it set's the data to that directly, circumventing the CORS policy

--- betaFiles/desktop/js/paperClip.js
+++ betaFiles/desktop/js/paperClip.js
@@ -1,5 +1,5 @@
 function search() {
     var search = document.getElementById("search");
     document.getElementById("body4").innerHTML=`<iframe style='width:100%; height:100%;' frameborder="0" id="theSiteThatYouWantToGoTo"></iframe>`;
-    document.getElementById("theSiteThatYouWantToGoTo").innerHTML=readDisk(search.value);
+    document.getElementById("theSiteThatYouWantToGoTo").contentDocument.write(readDisk(search.value));
 }
