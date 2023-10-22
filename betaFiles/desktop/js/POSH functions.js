7ab0a5fdffed4d88b98e17c0eeb299fdfe65c108
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-22 16:45:44 -0700

    moved the thing & renamed it

--- betaFiles/desktop/js/POSH functions.js
+++ betaFiles/desktop/js/POSH functions.js
@@ -671,6 +671,30 @@ if(running==true){
   if(event.button=="1")
 });*/
 
+//i found out you can create elements
+customElements.define('paperclp', class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.iframe = document.createElement('iframe');
+    this.iframe.src = this.getAttribute('src');
+    this.iframe.width = this.getAttribute('width');
+    this.iframe.height = this.getAttribute('height');
+    this.iframe.frameBorder = false;
+    this.appendChild(this.iframe);
+
+    this.browser = new RBI.Browser();
+
+    this.browser.on('ready', () => {
+      this.browser.load(this.iframe.src);
+    });
+
+    this.browser.on('data', (data) => {
+      this.iframe.contentDocument.body.innerHTML = data;
+    });
+  }
+});
+
 
 
 
--- betaFiles/desktop/js/paperClip.js
+++ betaFiles/desktop/js/paperClip.js
@@ -1,25 +1,3 @@
-customElements.define('paperclip', class extends HTMLElement {
-  constructor() {
-    super();
-
-    this.iframe = document.createElement('iframe');
-    this.iframe.src = this.getAttribute('src');
-    this.iframe.width = this.getAttribute('width');
-    this.iframe.height = this.getAttribute('height');
-    this.iframe.frameBorder = false;
-    this.appendChild(this.iframe);
-
-    this.browser = new RBI.Browser();
-
-    this.browser.on('ready', () => {
-      this.browser.load(this.iframe.src);
-    });
-
-    this.browser.on('data', (data) => {
-      this.iframe.contentDocument.body.innerHTML = data;
-    });
-  }
-});
 function search(){
-    document.getElementById("body4").innerHTML = `<paperlclip src="${document.getElementById("search").value}" width="100%" height="100%"></paperclip>`
+    document.getElementById("body4").innerHTML = `<paperlclp src="${document.getElementById("search").value}" width="100%" height="100%"></paperclp>`
 }
