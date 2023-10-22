82ba25adb854ea857ecce8604f91648d114c6d66
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-22 16:42:30 -0700

    ok, I hope this works, if it does, this will be HUGE.

--- betaFiles/desktop/js/paperClip.js
+++ betaFiles/desktop/js/paperClip.js
@@ -1,47 +1,25 @@
+customElements.define('paperclip', class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.iframe = document.createElement('iframe');
+    this.iframe.src = this.getAttribute('src');
+    this.iframe.width = this.getAttribute('width');
+    this.iframe.height = this.getAttribute('height');
+    this.iframe.frameBorder = false;
+    this.appendChild(this.iframe);
+
+    this.browser = new RBI.Browser();
+
+    this.browser.on('ready', () => {
+      this.browser.load(this.iframe.src);
+    });
+
+    this.browser.on('data', (data) => {
+      this.iframe.contentDocument.body.innerHTML = data;
+    });
+  }
+});
 function search(){
-var search = document.getElementById("search");
-var iframe = document.createElement("iframe");
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.frameBorder = "0";
-iframe.id = "theSiteThatYouWantToGoTo";
-document.getElementById("body4").appendChild(iframe);
-
-iframe.onload = function () {
-  var iframeDocument = iframe.contentDocument;
-  iframeDocument.open();
-
-  // Replace with the URL of the page you want to load
-  var url = "https://posh.glitch.me/";
-
-  // Make a request to the PHP proxy
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '../php/proxy.php?url=' + encodeURIComponent(url), true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-
-      iframeDocument.write(response.html);
-      
-      // Dynamically create script elements for the scripts
-      response.scripts.forEach(function (scriptSrc) {
-        var scriptElement = document.createElement("script");
-        scriptElement.src = scriptSrc;
-        iframeDocument.body.appendChild(scriptElement);
-      });
-
-      // Dynamically create link elements for the styles
-      response.styles.forEach(function (styleHref) {
-        var linkElement = document.createElement("link");
-        linkElement.rel = "stylesheet";
-        linkElement.type = "text/css";
-        linkElement.href = styleHref;
-        iframeDocument.head.appendChild(linkElement);
-      });
-
-      iframeDocument.close();
-    }
-  };
-  xhr.send();
-};
+    document.getElementById("body4").innerHTML = `<paperlclip src="${document.getElementById("search").value}" width="100%" height="100%"></paperclip>`
 }
