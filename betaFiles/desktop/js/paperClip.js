9323efec83780101a45aa3303bafc88b1586fe9e
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-22 17:00:39 -0700

    should work now

--- betaFiles/desktop/js/paperClip.js
+++ betaFiles/desktop/js/paperClip.js
@@ -1,3 +1,3 @@
 function search(){
-    document.getElementById("body4").innerHTML = `<paperlclp src="${document.getElementById("search").value}" width="100%" height="100%"></paperclp>`
+    document.getElementById("body4").innerHTML = `<paperlclp src="${document.getElementById("search").value}" width="100%" height="100%"></paperclp>`;
 }
