b7e767107b00b30fdf8ff3d4aaccc8f14199e304
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-22 17:01:49 -0700

    asdfasdfasdfasdfasdfasdf

--- betaFiles/desktop/php/server.php
+++ betaFiles/desktop/php/server.php
@@ -4,6 +4,8 @@
     </head>
     <body>
         <?php
+
+            ProxyPass /php/server.php /betaFiles/desktop/php/server.php
             $browser = new RBI.Browser();
 
             $browser->load($_GET['url']);
