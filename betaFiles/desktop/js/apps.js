362e9c0c7752fc3009fd63ee54a1d02239a3524d
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2023-10-23 19:24:37 -0700

    added the HTML file version of the apps because not only was there a ton of space, this also makes the apps.js file super tini

--- betaFiles/desktop/js/apps.js
+++ betaFiles/desktop/js/apps.js
@@ -10,16 +10,7 @@ function openPoster(data) {
 
 
 function openStickynote(data){
-    createWindow("stickynote", `
-    <link rel="stylesheet" href="../styles/stickynote.css">
-    <script src="../js/stickynote.js" defer></script>
-<div id="body2">
-    <div id="controlButtons">
-    <button id="saveFile" style="width:33.3%; height:1.5%;" onClick = "save()">save</button><button id="saveAsFile" style="width:33.3%; height:1.5%;">save as</button><button id="openFile" style="width:33.3%; height:1.5%;">open</button>
-    </div>
-    <textarea id="code">${data}</textarea>
-</div>
-`,"../icons/logos/stickynoteIcon.png");
+    createWindow("stickynote",readDisk("../html/stickynote.html"),"../icons/logos/stickynoteIcon.png");
 }
 
 function openPaperCLP(){
@@ -35,82 +26,16 @@ function time(){
 }
 
 function openPOSH(){
-    createWindow("POSH",`
-    <div id="bodyNumberTwo">
-    <link rel="stylesheet" href="../styles/POSH.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press Start 2P" defer/>
-    <script src="../js/POSH functions.js" defer></script>
-    <script src="../js/POSH.js" defer></script>
-    <ul id = "shell">
-    <pre  id="shellData">
-        <h4 class="user_disp">
-            POSH is broken, this can happen from a variety of reasons, one of those being that you're not using an HTTP server
-        </h4>
-    </pre>
-    <tag class="plag"></tag><input  type="text" name="shell" value="" spellcheck="false" id="input" contenteditable="true" autocomplete="off">
-    <br>
-    <br>
-    <br>
-
-    
-</ul>
-
-
-</div>
-    `,"../icons/logos/console logo.png")
+    createWindow("POSH",readDisk("../html/POSH.html"),"../icons/logos/console logo.png")
 }
 
 
 function openFolders(location){
-       createWindow("bookshelf", `
-       <link rel="Stylesheet" href="../styles/bookshelf.css"/>
-       <script src="../js/bookshelf.js" defer></script>
-       <script>
-       function goBack() {
-        var locationElement = document.getElementById("locat_ion");
-        var currentPath = locationElement.innerHTML;
-    
-        // Split the current path by '/'
-        var pathParts = currentPath.split('/');
-    
-        if (pathParts.length > 1) {
-            // If there are more than one parts, remove the last one and update the innerHTML
-            pathParts.pop(); // Remove the last part
-            locationElement.innerHTML = pathParts.join('/');
-        } else {
-            console.log("no");
-        }
-    }
-       </script>
-       <div id="controlContainer" style=" position:absolute; top:0; background-color:rgb(35,35,35); width:100%; height:2.5vw; z-index:5;">
-       <h3 id="locat_ion">${location}</h3>
-        <button id=".." class="FileButton" onClick = 'goBack(); load();'></button>
-       </div>
-    <div id="listOfFiles" style='background-color:rgb(55,55,55); color:white; width:100%; height:100%;'>
-    <br><br>
-    <div id="files">
-
-    </div>
-    </div>
-    `,"../icons/logos/bookshelf.png");
+       createWindow("bookshelf",readDisk("bookshelf.html") ,"../icons/logos/bookshelf.png");
 }
 
 function openSettings(){
-    createWindow("settings", `
-    
-    <link rel="Stylesheet" href="settings.css"/>
-    <script src="../js/settings.js">
-    <div id="background">
-    <center>
-        <h1>settings</h1>
-        <button class="settingButton">change background/theme</button>
-        <div id="subsetting">
-
-        </div>
-    </center>
-
-    </div>
-    `)
+    createWindow("settings", readDisk("settings.html"))
 }
 
 function Scroll(){
